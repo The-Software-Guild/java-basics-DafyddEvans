9d9dd84ec829bf78ee3d3a932b4025254385c3ae
Dafydd Pierce-Evans <dpierceevans@gmail.com>
2021-10-01 13:47:00 +0100

    006: added input validation

--- src/main/java/org/dafydd/c130/exercises/HealthyHearts.java
+++ src/main/java/org/dafydd/c130/exercises/HealthyHearts.java
@@ -7,12 +7,25 @@ public class HealthyHearts {
 
     public static void main(String[] args) {
         int age, maximumHR;
+        age = 0;
         long lowRange, highRange;
+        boolean isValid = false;
 
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.print("What is your age? ");
-        age = myScanner.nextInt();
+        while(!isValid) {
+            System.out.print("How old are you?: ");
+
+            try {
+                age = Integer.parseInt(myScanner.nextLine());
+                isValid = true;
+            }
+            catch (NumberFormatException ex) {
+                System.out.println("Invalid input type, please use numbers only.\n");
+            }
+
+        }
+
         maximumHR = 220 - age;
         lowRange = Math.round(maximumHR * 0.5);
         highRange = Math.round(maximumHR * 0.85);
